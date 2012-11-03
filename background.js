97763ba457ef58e7dfc3ca0ddc6cdb1f123be3eb
Johnner <jhnner@gmail.com>
2012-11-03 17:52:26 +0400

    resource files

---
+++ background.js
@@ -0,0 +1,42 @@
+var unCache = {
+    checkCacheCleared: function () {
+        console.log('Clearing cache...');
+        this.clearCache();
+    },
+
+    clearCache: function () {
+        var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+        var oneWeekAgo = (new Date()).getTime() - millisecondsPerWeek;
+        chrome.browsingData.remove({
+            "since": oneWeekAgo
+        }, {
+            "appcache": true,
+            "cache": true,
+            "cookies": false,
+            "downloads": false,
+            "fileSystems": true,
+            "formData": false,
+            "history": false,
+            "indexedDB": true,
+            "localStorage": true,
+            "pluginData": false,
+            "passwords": false,
+            "webSQL": false
+        });
+        chrome.browserAction.setIcon({
+            path: "logo_clean.png"
+        });
+
+        _gaq.push(['_trackEvent', 'clearCache', 'clicked']);
+    },
+
+    storageChanged: function (history_item) {
+        chrome.browserAction.setIcon({
+            path: "logo.png"
+        });
+    }
+};
+
+chrome.browserAction.onClicked.addListener(unCache.checkCacheCleared.bind(unCache));
+chrome.history.onVisited.addListener(unCache.storageChanged.bind(unCache));
+//chrome.storage.onChanged.addListener();
