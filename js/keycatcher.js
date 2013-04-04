2b8ce811c5315c3b0466adade5a80c5815077eee
Johnner <jhnner@gmail.com>
2013-04-04 21:25:04 +0400

    bug fix

--- js/keycatcher.js
+++ js/keycatcher.js
@@ -1,3 +1,4 @@
+
 (function () {
     var storage = chrome.storage.sync,
         pattern = [],
@@ -22,13 +23,15 @@
     function keySender (e) {
         pressed++;
         var keyCode = e.keyCode || e.which;
-        if (pattern.indexOf(keyCode) != -1) {
-            matched++;
-        }
-        if (pressed == pattern.length && pressed == matched) {
-            chrome.extension.sendMessage({clear: 1});
-            matched = 0;
-            pressed = 0;
+        if (pattern) {
+            if (pattern.indexOf(keyCode) != -1) {
+                matched++;
+            }
+            if (pressed == pattern.length && pressed == matched) {
+                chrome.extension.sendMessage({clear: 1});
+                matched = 0;
+                pressed = 0;
+            }
         }
     }
 
@@ -36,11 +39,13 @@
     function keyUp (e) {
         pressed = 0;
         var keyCode = e.keyCode || e.which;
-        if (pattern.indexOf(keyCode) !== -1) {
-            matched = 0;
-        }
-        if (pressed == 0) {
-            matched = 0;
+        if (pattern) {
+            if (pattern.indexOf(keyCode) !== -1) {
+                matched = 0;
+            }
+            if (pressed == 0) {
+                matched = 0;
+            }
         }
     }
 
